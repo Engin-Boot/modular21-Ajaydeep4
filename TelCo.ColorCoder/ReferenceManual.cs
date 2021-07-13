d1649b022d5116cd3d40777cdc51b9ea7940130f
Ajaydeep4 <46348665+Ajaydeep4@users.noreply.github.com>
2021-07-13 18:12:16 +0530

    added manual

---
+++ TelCo.ColorCoder/ReferenceManual.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TelCo.ColorCoder
+{
+    public class ReferenceManual
+    {
+        public static void Manual()
+        {
+        int pair_no = 1;
+            int maj_size = PairsDataModel.colorMapMajor.Length  ;
+            int min_size = PairsDataModel.colorMapMajor.Length;
+            for(int i=0; i<maj_size; ++i)
+                for(int j=0; j<min_size; ++j)
+                {
+                Console.WriteLine("Major-color : {0}, Minor-color : {1}, Pair : {2}", PairsDataModel.colorMapMajor[i], PairsDataModel.colorMapMajor[j], pair_no);
+                }
+
+    }
+
+}
+}
