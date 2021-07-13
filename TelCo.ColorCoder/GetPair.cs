c881e893c84c75a17114e498771225f836b8f441
Ajaydeep4 <46348665+Ajaydeep4@users.noreply.github.com>
2021-07-13 17:06:29 +0530

    updated for word limit

---
+++ TelCo.ColorCoder/GetPair.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TelCo.ColorCoder
+{
+    public class GetPair
+    {
+
+        /// <summary>
+        /// Given the two colors the function returns the pair number corresponding to them
+        /// </summary>
+        /// <param name="pair">Color pair with major and minor color</param>
+        /// <returns></returns>
+        public static int GetPairNumberFromColor(PairsDataModel.ColorPair pair)
+        {
+            // Find the major color in the array and get the index
+            int majorIndex = -1;
+            for (int i = 0; i < PairsDataModel.colorMapMajor.Length; i++)
+            {
+                if (PairsDataModel.colorMapMajor[i] == pair.majorColor)
+                {
+                    majorIndex = i;
+                    break;
+                }
+            }
+
+            // Find the minor color in the array and get the index
+            int minorIndex = -1;
+            for (int i = 0; i < PairsDataModel.colorMapMinor.Length; i++)
+            {
+                if (PairsDataModel.colorMapMinor[i] == pair.minorColor)
+                {
+                    minorIndex = i;
+                    break;
+                }
+            }
+            // If colors can not be found throw an exception
+            if (majorIndex == -1 || minorIndex == -1)
+            {
+                throw new ArgumentException(
+                    string.Format("Unknown Colors: {0}", pair.ToString()));
+            }
+
+            // Compute pair number and Return  
+            // (Note: +1 in compute is because pair number is 1 based, not zero)
+            return (majorIndex * PairsDataModel.colorMapMinor.Length) + (minorIndex + 1);
+        }
+    }
+
+}
